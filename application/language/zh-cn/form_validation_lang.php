bb6c4b5a45b7de3360cdfcc654951d75eca65287
anpone <anpone@gmail.com>
2016-01-11 09:59:47 +0800

    App
    APPLICATION ENVIRONMENT

---
+++ application/language/zh-cn/form_validation_lang.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author	CodeIgniter community
+ * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+$lang['form_validation_required'] = '要求含有 {field} 字段';
+$lang['form_validation_isset'] = '{field} 字段必须有值';
+$lang['form_validation_valid_email'] = '{field} 字段必须是一个有效的 E-mail 地址';
+$lang['form_validation_valid_emails'] = '{field} 字段必须包含有效的 E-mail地址';
+$lang['form_validation_valid_url'] = '{field} 字段必须是一个有效的 URL';
+$lang['form_validation_valid_ip'] = '{field} 字段必须包含一个有效的 IP 位址';
+$lang['form_validation_min_length'] = '{field} 字段最少需要有 {param} 字符';
+$lang['form_validation_max_length'] = '{field} 字段不能超过 {param} 字符';
+$lang['form_validation_exact_length'] = '{field} 字段必须是 {param} 字符';
+$lang['form_validation_alpha'] = '{field} 字段取值只允许为字母';
+$lang['form_validation_alpha_numeric'] = '{field} 字段取值只允许为字母和数字字符';
+$lang['form_validation_alpha_numeric_spaces'] = '{field} 字段取值只允许为字母、数位和空格';
+$lang['form_validation_alpha_dash'] = '{field} 字段取值只允许为字母、数位、底线和破折号';
+$lang['form_validation_numeric'] = '{field} 字段取值只允许为数字';
+$lang['form_validation_is_numeric'] = '{field} 字段必须只包含数字字符';
+$lang['form_validation_integer'] = '{field} 字段必须是整数';
+$lang['form_validation_regex_match'] = '{field} 字段的格是错误';
+$lang['form_validation_matches'] = '{field} 字段与 {param} 字段不符';
+$lang['form_validation_differs'] = '{field} 字段与 {param} 字段必须不同';
+$lang['form_validation_is_unique'] = '{field} 字段必须是一个独一无二的值';
+$lang['form_validation_is_natural'] = '{field} 字段必须是自然数';
+$lang['form_validation_is_natural_no_zero'] = '{field} 字段必须是非 0 的自然数';
+$lang['form_validation_decimal'] = '{field} 字段必须是十进位数字';
+$lang['form_validation_less_than'] = '{field} 字段的值必须小于 {param}';
+$lang['form_validation_less_than_equal_to'] = '{field} 字段的值必须小于等于 {param}';
+$lang['form_validation_greater_than'] = '{field} 字段的值必须大于 {param}';
+$lang['form_validation_greater_than_equal_to'] = '{field} 字段的值必须大于等于 {param}';
+$lang['form_validation_error_message_not_set'] = '无法取得 {field} 字段的错误信息';
+$lang['form_validation_in_list'] = '{field} 字段必须是 {param} 中的一种';
---
+++ application/language/zh-cn/ftp_lang.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author	CodeIgniter community
+ * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+$lang['ftp_no_connection'] = '无法找到一个有效的连线 ID。在执行任何文件传输时请确认已经连线成功';
+$lang['ftp_unable_to_connect'] = '无法使用提供的主机名称连接到 FTP 服务器';
+$lang['ftp_unable_to_login'] = '无法登入 FTP 服务器，请检查使用者名称及密码是否正确';
+$lang['ftp_unable_to_mkdir'] = '无法建立指定的文件夹';
+$lang['ftp_unable_to_changedir'] = '无法切换目录';
+$lang['ftp_unable_to_chmod'] = '无法设定文件权限，请确认检查文件路径或使用者权限';
+$lang['ftp_unable_to_upload'] = '无法上传指定的文件，检查上传路径或使用者权限';
+$lang['ftp_unable_to_download'] = '无法下载指定的文件，检查下载路径或使用者权限';
+$lang['ftp_no_source_file'] = '无法找到指定的原始文件，请检查文件路径或使用者权限';
+$lang['ftp_unable_to_rename'] = '无法重新命名';
+$lang['ftp_unable_to_delete'] = '无法删除文件';
+$lang['ftp_unable_to_move'] = '无法移动文件，请检查目标文件夹是否存在';
